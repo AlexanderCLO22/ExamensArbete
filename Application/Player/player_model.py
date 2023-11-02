4da087ec10445466c55c2e3b2604360ba9707f86
Alexander Öjersson <98aleoje@gafe.molndal.se>
2023-11-02 17:33:13 +0100

    first

---
+++ Application/Player/player_model.py
@@ -0,0 +1,4 @@
+class Player ():
+    username = ""
+    password = ""
+    email = ""
---
+++ __init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    print(__name__)
+    
+    return app
